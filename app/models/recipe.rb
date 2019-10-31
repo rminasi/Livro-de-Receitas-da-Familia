7f5aa64e0a421cdfc51120ae7b10c33b437d7eb5
Raquel Minasi <raquelminasi@gmail.com>
2019-10-31 21:40:28 +0000

    new and show page created

--- app/models/recipe.rb
+++
@@ -1,6 +0,0 @@
-class Recipe < ApplicationRecord
-  mount_uploader :photo, PhotoUploader
-  has_many :ingredients
-  # accepts_nested_attributes_for :ingredients
-  validates :name, uniqueness: true, presence: true
-end
